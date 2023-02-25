27c755cb30ac2e4ba8688d749d4a7c77ecbfc886
Ashish Mishra <ashishmishra9467@gmail.com>
2023-02-25 20:41:37 +0530

    done

--- advance-auto-parts/src/App.js
+++ advance-auto-parts/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { AllRoutes } from './Components/AllRoutes/AllRoutes';
 import Home from './Home';
+import Navbar from './Components/Navbar';
+
 
 function App() {
   return (
     <div style={{backgroundColor:"#373737"}}>
-     <Home/>
-     {/* <AllRoutes/> */}
+       <Navbar/>
+     {/* <Home/> */}
+     <AllRoutes/>
     </div>
   );
 }
